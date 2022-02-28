1419eefb8dada29bd94763b7e626380815b3ab41
John Lathouwers <john.lathouwers@oracle.com>
2022-02-28 09:06:09 +0000

    Add delete, vars from config, UploadManager

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ objstore_backup.py uses the [OCI Python SDK](https://oracle-cloud-infrastructure
 The OCI Module must be installed and can be done either Globally or in a Virtual Environment (Recommended)
 
 ### Python Virtual Environment
-On your linux machine, as the user running the script, create a python virtual environment and install the OCI python module
+On your linux machine, as the user running the script, create a python virtual environment and install the OCI python module.  This is a one-off setup:
 ```
 python3 -m venv .venv
 source .venv/bin/activate
@@ -20,8 +20,18 @@ pip install oci
 source .venv/bin/activate
 ```
 
+To ensure that the above virtual environment is always used by the running OS user, activate with the users shell "rc" file; for example on bash:
+
+Add to ~/.bashrc
+```
+# Source python virtual environment
+if [ -f $HOME/.venv/bin/activate ]; then
+        . $HOME/.venv/bin/activate
+fi
+```
+
 ### Globally 
-As the root user:
+If not using the Python Virtual Environment,to install the OCI module globally, as the root user:
 ```
 pip install --upgrade pip wheel
 pip install oci
@@ -51,6 +61,53 @@ Update the permissions on your downloaded private key file so that only you can 
 1. Go to the .oci directory where you placed the private key file.
 2. Use the command chmod go-rwx ~/.oci/<oci_api_keyfile>.pem to set the permissions on the file.
 
+### Advanced Configuration
+The following script input variables can be specified in the config file to avoid having to input on the command line:
+* bucket
+* proxy
+* src
+* dst
+
+For example:
+```
+[DEFAULT]
+compartment=ocid1.compartment.oc1..
+user=ocid1.user.oc1..
+fingerprint=<fingerpring>
+tenancy=ocid1.tenancy.oc1..
+region=<region>
+key_file=~/.oci/key.pem
+proxy=myproxy-host.example.com:4889
+bucket=FILESYSTEM
+src=/u01/app/datapump/db1
+dst=/u01/app/datapump/db1_new
+```
+
+Additionally, multiple profiles can be used for standard variations to the inputs.  For common configurations, such as the user, tenancy, fingerprint, proxy, etc. use `DEFAULT`; create new profiles for the variables:
+
+For example:
+```
+[DEFAULT]
+compartment=ocid1.compartment.oc1..
+user=ocid1.user.oc1..
+fingerprint=<fingerpring>
+tenancy=ocid1.tenancy.oc1..
+region=<region>
+key_file=~/.oci/key.pem
+proxy=myproxy-host.example.com:4889
+
+[PROD]
+bucket=PROD_FS
+src=/u01/app/datapump/prod
+dst=/u01/app/datapump/prod_new
+
+[DEV]
+bucket=DEV_FS
+src=/u01/app/datapump/dev
+dst=/u01/app/datapump/dev_new
+```
+
+Use `-t PROD` or `-t DEV` in the script to consume and set the script inputs.  What is not found in those configuration profiles will be obtained from the `DEFAULT`.
 
 # Example Usage:
 ```
@@ -61,8 +118,8 @@ usage: objstore_backup.py [-h] -a {upload,download,list} -b BUCKET
 Object Store Filesystem Utilities
 
 Required arguments:
-  -a {upload,download,list} : Action
-  -b BUCKET                 : Bucket Name
+  -a {upload,download,delete,list} : Action
+  -b BUCKET                        : Bucket Name
 
 Optional arguments:
   -c CONFIG_FILE        Config File (default=~/.oci/config)
@@ -76,19 +133,26 @@ Action Specific (upload/download) arguments:
 
 ## Upload Files/Directories to Object Storage
 * Upload Recursive Directory: 
-    * `python ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump`
+    * `python3 ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump`
 * Upload Single File: 
-    * `python ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1/file01.dmp`
+    * `python3 ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1/file01.dmp`
 
 ## List Object Storage Files
 * List all Object Storage: 
-    * `python ./objstore_upload.py -a list -t cloud-backup-demo -b FILESYSTEM`
+    * `python3 ./objstore_upload.py -a list -t cloud-backup-demo -b FILESYSTEM`
 * List subset Object Storage: 
-    * `python ./objstore_upload.py -a list -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1`
+    * `python3 ./objstore_upload.py -a list -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1`
 
 ## Download
 Use the -a list to find the full path of the object storage file, then pass to -s; use -d to place in a different local directory (avoid overwriting)
 * Download Recursive Directory: 
-    * `python ./objstore_backup.py -a download -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1 -d /u01/app/datapump/db1_new`
+    * `python3 ./objstore_backup.py -a download -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1 -d /u01/app/datapump/db1_new`
 * Download Single File: 
-    * `python ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1/file01.dmp -d /u01/app/datapump/db1_new/file01.dmp`
+    * `python3 ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1/file01.dmp -d /u01/app/datapump/db1_new/file01.dmp`
+
+## Delete
+Use the -a list to find the full path of the object storage file, then pass to -s
+* Delete Recursive Directory: 
+    * `python3 ./objstore_backup.py -a delete -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1`
+* Download Single File: 
+    * `python3 ./objstore_backup.py -a upload -t cloud-backup-demo -b FILESYSTEM -s /u01/app/datapump/db1/file01.dmp`
--- objstore_backup.py
+++ objstore_backup.py
@@ -1,10 +1,13 @@
+#!/bin/env python3
+
 # Copyright (c) 2016, 2022, Oracle and/or its affiliates.  All rights reserved.
 # This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
-
 import oci
 import os, argparse, stat
-from multiprocessing import Process
-import glob
+import glob, time, math, multiprocessing
+from datetime import timedelta
+from oci.object_storage.transfer.constants import MEBIBYTE
+
 
 ##########################################################################
 # Print header centered
@@ -16,18 +19,32 @@ def print_header(msg):
     print("#" + msg.center(chars - 2, " ") + "#")
     print('#' * chars)
 
+
+##########################################################################
+# Print header centered
+##########################################################################
+def set_value(key, arg, config):
+  value = arg
+  if not arg:
+    try:
+      value = config[key]
+    except KeyError:
+      value = None
+  return value
+
+
 ##############################################################################
 # get_namespace
 ##############################################################################
 def get_namespace(client):
   print_header("Connecting to Object Storage")
   try:
-    namespace = client.get_namespace(retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY).data
+    namespace = client.get_namespace().data
     print("Succeeded - Namespace = {}".format(namespace))
   except Exception as e:
-      print("\nError connecting to object storage - {}".format(e))
-      raise SystemExit
+      raise SystemExit("\nError connecting to object storage - {}".format(e))
   return namespace
+
 
 ##############################################################################
 # list_object_storage
@@ -36,7 +53,10 @@ def list_object_storage(client, namespace, bucket, src=None):
   object_dict = {}
   next_starts_with = None
   while True:
-    response = client.list_objects(namespace, bucket, start=next_starts_with, prefix=None, fields='size', retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+    try:
+      response = client.list_objects(namespace, bucket, start=next_starts_with, prefix=None, fields='size')
+    except Exception as e:
+      raise SystemExit(e.message)
     next_starts_with = response.data.next_start_with
     for object_file in response.data.objects:
       if src and not object_file.name.startswith(src):
@@ -48,21 +68,42 @@ def list_object_storage(client, namespace, bucket, src=None):
   return object_dict
 
 ##############################################################################
+# delete_from_object_storage
+##############################################################################
+def delete_from_object_storage(client, namespace, bucket, path):
+  print("Deleting {} from {}".format(path, bucket), end=": ", flush=True)
+  start_time = time.time()
+  client.delete_object(namespace, bucket, path)
+  print(str(timedelta(seconds=time.time() - start_time)))
+
+##############################################################################
 # upload_to_object_storage
 ##############################################################################
-def upload_to_object_storage(client, namespace, bucket, path):
+def upload_to_object_storage(client, namespace, bucket, path, cpu=1):
+  # Note using UploadManager instead of put_object for multipart
+  file_size = os.stat(path).st_size
+  if file_size > (1024 * MEBIBYTE):
+    part_bytes = math.floor(file_size/(35*cpu))
+  else:
+      part_bytes = file_size
+
   try:
-    with open(path, "rb") as in_file:
-      # name is the full path in ObjectStore; change as required
-      name = path     
-      client.put_object(namespace, bucket, name, in_file)
-      print("Finished uploading {}".format(path))
+    part_count = round(file_size/part_bytes)
+  except ZeroDivisionError:
+    part_count = 1
+
+  try:
+    print("Uploading {} [{} part(s)]".format(path,part_count), end=": ", flush=True)
+    start_time = time.time()
+    client.upload_file(namespace, bucket, path, path, part_size=part_bytes)
+    print(str(timedelta(seconds=time.time() - start_time)))
   except PermissionError:
-    print("Failed to upload {} - Unable to read local file".format(path))
+    print("Failed - Unable to read local file")
   except FileNotFoundError:
-    print("Failed to upload {} - File Not Found".format(path))
+    print("Failed - File Not Found")
   except OSError as e:
-    print("Failed to upload {} - {}".format(path, e))
+    print("Failed - {}".format(e))
+
 
 ##############################################################################
 # download_from_object_storage
@@ -76,28 +117,26 @@ def download_from_object_storage(client, namespace, bucket, path, out_file=None)
 
   os.makedirs(os.path.dirname(out_file), exist_ok=True)
 
+  print("Downloading {} to {}".format(path, out_file), end=": ", flush=True)
+  start_time = time.time()
   with open(out_file, 'wb') as f:
     for chunk in get_obj.data.raw.stream(1024 * 1024, decode_content=False):
         f.write(chunk)
-    print("Finished downloading {} to {}".format(path, out_file))
+    print(str(timedelta(seconds=time.time() - start_time)))
 
 ##############################################################################
 # MAIN
 ##############################################################################
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Object Store Filesystem Utilities')
-  parser.add_argument('-a', required=True,  dest='action', choices=['upload', 'download','list'], help="Action")
-  parser.add_argument('-b', required=True,  dest='bucket', help='Bucket Name')
+  parser.add_argument('-a', required=True,  dest='action', choices=['upload', 'download','list','delete'], help="Action")
+  parser.add_argument('-b', required=False, default=None, dest='bucket', help='Bucket Name')
   parser.add_argument('-c', required=False, dest='config_file', help="Config File (default=~/.oci/config)")
   parser.add_argument('-t', required=False, dest='config_profile', help='Config file section to use (DEFAULT)')
-  parser.add_argument('-p', required=False, dest='proxy', help='Set Proxy (i.e. www-proxy-server.com:80) ')
-  parser.add_argument('-s', required=False, default="", dest='src', help="Source path")
-  parser.add_argument('-d', required=False, default="", dest='dst', help="Destination path")
-
+  parser.add_argument('-p', required=False, default=None, dest='proxy', help='Set Proxy (i.e. www-proxy-server.com:80) ')
+  parser.add_argument('-s', required=False, default=None, dest='src', help="Source path")
+  parser.add_argument('-d', required=False, default=None, dest='dst', help="Destination path")
   args = parser.parse_args()
-
-  # Potentially will modify later
-  src = args.src
 
   # Update Variables based on the parameters
   config_file = (args.config_file if args.config_file else oci.config.DEFAULT_LOCATION)
@@ -108,56 +147,69 @@ if __name__ == "__main__":
       (config_file if config_file else oci.config.DEFAULT_LOCATION),
       (config_profile if config_profile else oci.config.DEFAULT_PROFILE))
   except Exception as e:
-    print(e)
-    raise SystemExit
+    raise SystemExit(e)
 
+  # Test for input overrides and config values
+  bucket = set_value('bucket', args.bucket, config)
+  proxy  = set_value('proxy', args.proxy, config)
+  src    = set_value('src', args.src, config)
+  dst    = set_value('dst', args.dst, config)
+
+  if not bucket:
+    raise SystemExit('-b <bucket> is requiered')
+  if args.action == 'upload' and not src:
+    raise SystemExit('For -a upload, -s <src> is required')
+  if args.action == 'download' and not src and not dst:
+    raise SystemExit('For -a download, -s <src> and -d <dst> is required')
+    
   # Establish the ObjStorage Client
-  object_storage_client = oci.object_storage.ObjectStorageClient(config)
-  if args.proxy:
-    object_storage_client.base_client.session.proxies = {'https': args.proxy}
+  object_storage_client = oci.object_storage.ObjectStorageClient(config, retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+  if proxy:
+    object_storage_client.base_client.session.proxies = {'https': proxy}
 
   namespace = get_namespace(object_storage_client)
 
-  filenames = {}
+  # Get a list of files in the bucket (this also verifies the existance of bucket)
+  objects = {}
+  objects = list_object_storage(object_storage_client, namespace, bucket, src)
+
   if args.action == 'list':
-    print_header("Listing Objects in {} Bucket".format(args.bucket))
-    filenames = list_object_storage(object_storage_client, namespace, args.bucket, src)
-    for file in filenames:
-      print(str('{:10,.0f}'.format(filenames[file])).rjust(10) + " - " + str(file))
+    print_header("Listing Objects in {} Bucket".format(bucket))
+    for file in objects:
+      print(str('{:10,.0f}'.format(objects[file])).rjust(10) + " - " + str(file))
+
+  if args.action == 'delete':
+    print_header("Deleting from Object Storage")
+    for file in objects:
+      delete_from_object_storage(object_storage_client, namespace, bucket, file)
 
   if args.action == 'download':
     print_header("Downloading from Object Storage")
-    filenames = list_object_storage(object_storage_client, namespace, args.bucket, src)
-    for file in filenames:
-      download_from_object_storage(object_storage_client, namespace, args.bucket, file, args.dst)
+    for file in objects:
+      download_from_object_storage(object_storage_client, namespace, bucket, file, dst)
 
   if args.action == 'upload':
-    print_header("Uploading to Object Storage")
+    parallel = multiprocessing.cpu_count()
+    print('Setting Upload Parallelism to {}'.format(parallel))
+    files = {}
     if os.path.isdir(src):
       if not src.endswith('/'):
         src = src + '/'
       print('Getting files in {}'.format(src))
       for file in glob.glob(src + "**/**", recursive=True):
-        filenames[os.path.abspath(file)] = "local"
+        files[os.path.abspath(file)] = "local"
     else:
-      filenames[os.path.abspath(src)] = "local"
+      files[os.path.abspath(src)] = "local"
 
-    proc_list = []
-    for filePath in filenames:
+    print_header("Uploading {} local files to Object Storage".format(len(files)))
+    upload_manager = oci.object_storage.UploadManager(
+      object_storage_client, allow_parallel_uploads=True, parallel_process_count=parallel)
+    for filePath in files:
       if os.path.isdir(filePath):
         continue
       # Skip socket files
       mode = os.stat(filePath).st_mode
       if stat.S_ISSOCK(mode) or stat.S_ISFIFO(mode):
         continue
-
       full_path=os.path.abspath(filePath)
-      print("Starting upload for {}".format(full_path))
-      # Playing around with parallelism... to serialise, uncomment below and comment out everything after
-      # upload_to_object_storage(object_storage_client, namespace, args.bucket, full_path)
-      p = Process(target=upload_to_object_storage, args=(object_storage_client, namespace, args.bucket, full_path))
-      p.start()
-      proc_list.append(p)
-
-    for job in proc_list:
-        job.join()
+      upload_to_object_storage(upload_manager, namespace, bucket, full_path, parallel)
